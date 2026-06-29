c65fa6032f8f7aee68a24af413ec855eff92707f
MilkywayRides <milkywayrides@gmail.com>
2026-06-29 12:00:42 +0530

    Remove sentry test page

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,3 @@
-import "./instrumentation-client";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider"
--- frontend/app/sentry-test/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function SentryTest() {
-  return (
-    <button onClick={() => { throw new Error("Frontend Sentry test error"); }}>
-      Trigger Error
-    </button>
-  );
-}
